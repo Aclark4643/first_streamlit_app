1bda29e380d5a70c034730b3af99d3b9ad42a3bc
Aclark4643 <140985118+Aclark4643@users.noreply.github.com>
2023-07-30 19:33:15 -0400

    Update streamlit_app.py
    removed capital letter from line 5 Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-Streamlit.title("Breakfast Menu")
+streamlit.title("Breakfast Menu")
 streamlit.title("Omega 3 & Blueberry Oatmeal"0
